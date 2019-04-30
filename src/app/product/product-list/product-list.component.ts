a22ab4ac29793713a13a3d69f569ac6657c720aa
Kiyotaka <>
2019-04-30 22:36:11 +0900

    Add product service

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,16 +9,14 @@ import { Product } from '../../shared/models/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = null
-  constructor() { }
+
+  constructor(
+    private productService: ProductService,
+  ) {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.products = [
-        new Product(1, 'Angular入門書', 3800, 'Angular入門書の説明'),
-        new Product(2, 'Angular覚えたら人生変わった！', 410, '年収300万のSEが、Angularと出会う。それは、小さな会社の社畜が始めた、最初の抵抗だった。'),
-        new Product(3, '異世界転生から始めるAngular生活', 680, 'スパゲッティの沼でデスマーチ真っ最中の田中。過酷な日々からの現実逃避か彼は、異世界に放り出され、そこでAngularの入門書を拾う。現実逃避でさえ、プログラミングをするしかない彼に待ち受けるのは！？'),
-      ];
-    }, 3000);
+    this.productService.list().subscribe((products: Product[]) => {
+      this.products = products;
+    });
   }
-
 }
---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { Observable, of } from 'rxjs/index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  products = [
+    new Product(1, 'Angular入門書', 3800, 'Angular入門書の説明'),
+    new Product(2, 'Angular覚えたら人生変わった！', 410, '年収300万のSEが、Angularと出会う。それは、小さな会社の社畜が始めた、最初の抵抗だった。'),
+    new Product(3, '異世界転生から始めるAngular生活', 680, 'スパゲッティの沼でデスマーチ真っ最中の田中。過酷な日々からの現実逃避か彼は、異世界に放り出され、そこでAngularの入門書を拾う。現実逃避でさえ、プログラミングをするしかない彼に待ち受けるのは！？'),
+  ];
+
+  constructor() { }
+
+  list(): Observable<Product[]> {
+    return of(this.products)
+  }
+}
